d7f3272ba17e7465f7532c3824ca6c22d6a2ff15
Quentin Burg <quentin@marigold.dev>
2023-12-08 17:52:06 +0100

    :recycle: update tutorial

--- src/lib/MintingComponent.svelte
+++ src/lib/MintingComponent.svelte
@@ -17,20 +17,7 @@
 
   function mint(user_address: string) {
       (async () => {
-          const gas_api = new GasStation({
-            apiURL: PUBLIC_GAS_STATION_API
-          });
-          const contract = await Tezos.wallet.at(PUBLIC_PERMIT_CONTRACT);
-          const mint_op = await contract.methodsObject.mint_token([{
-              owner: user_address,
-              token_id: tokenId,
-              amount_: 1
-          }]).toTransferParams()
-          console.log(mint_op);
-          const response = await gas_api.postOperation(user_address, {
-            destination: mint_op.to,
-            parameters: mint_op.parameter
-          });
+          // TODO: Write here the MINT function
       })();
   }
 
--- src/lib/StakingComponent.svelte
+++ src/lib/StakingComponent.svelte
@@ -29,58 +29,12 @@
         console.error("No wallet");
         return;
       }
-
-      const gas_api = new GasStation({
-        apiURL: PUBLIC_GAS_STATION_API
-      });
-      const permit_contract = new PermitContract(PUBLIC_PERMIT_CONTRACT, Tezos);
-
-
-      const permit_data = await permit_contract.generatePermit({
-        from_: user_address,
-        txs: [{
-          to_: PUBLIC_STAKING_CONTRACT,
-          token_id: tokenId,
-          amount: 1
-        }]
-      });
-
-      const signature = (await (await wallet?.client)?.requestSignPayload({
-          signingType: SigningType.MICHELINE,
-          payload: permit_data.bytes
-      }))?.signature;
       const activeAccount = await wallet?.client.getActiveAccount();
-
       if (!activeAccount) {
         throw new Error('No active account, cannot stake')
       }
 
-      const permit_op = await permit_contract.permitCall({
-          publicKey: activeAccount.publicKey,
-          signature: signature,
-          transferHash: permit_data.transfer_hash
-      });
-      console.log(permit_op);
-      console.log("ok");
-      const staking_contract = await Tezos.wallet.at(PUBLIC_STAKING_CONTRACT);
-      const staking_op = await staking_contract.methods.stake(
-        1,
-        user_address
-      ).toTransferParams();
-
-      const response = await gas_api.postOperations(user_address,
-          [
-            {
-              destination: permit_op.to,
-              parameters: permit_op.parameter
-            },
-            {
-              destination: staking_op.to,
-              parameters: staking_op.parameter
-            }
-          ]);
-
-      console.log(response);
+      // TODO: Write here the staking function
     })();
   }
 
